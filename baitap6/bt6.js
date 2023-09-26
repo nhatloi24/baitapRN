13d22e38b80fd20bce98588766f685932748b1ad
nhatloi24 <nhxloi24@gmail.com>
2023-09-26 22:29:16 +0700

    source code bt 6

---
+++ baitap6/bt6.js
@@ -0,0 +1,64 @@
+// bai 1
+// function timSoLonNhat(num1, num2, num3){
+
+//     if (num1 > num2 && num1 > num3){
+//         console.log('Max:', num1);
+//     } else if (num2 > num1 && num2 > num3){
+//         console.log('Max:', num2)
+//     } else {
+//         console.log('Max:', num3)
+//     }
+
+// }
+// timSoLonNhat(5, 8, 33);
+
+// bai 2
+
+// let kiemtraSoChan = (a) => {
+//     if (a % 2 === 0){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+// console.log("check:", kiemtraSoChan(10));
+
+//bai 3
+
+// let arr = [
+//     'Cam',
+//     'Xoai',
+//     'Tao',
+//     'Oi',
+//     'Man',
+// ];
+// console.log('Qua:', arr[3]);
+
+//bai 4
+// let languages = [
+//     "Java",
+//     "Python",
+//     "Javascript",
+//     "PHP",
+//     "Ruby",
+// ];
+
+// let fruits = [
+//     'Mango',
+//     'Orange',
+// ];
+//  languages.splice(1, 1 , 'C', 'C++');
+//  languages.splice(0, 0, 'C', 'C++' , '.Net');
+// let delLanguage =languages.pop();
+// let newArr = languages.concat(fruits);
+// languages.reverse();
+// languages.length;
+
+
+// console.log(">>> New Arr:", newArr);
+
+// console.log(languages);
+// console.log(delLanguage);
+
+
+
