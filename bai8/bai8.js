d608e367de9cc6be0bb0b078595982fd969fb568
nhatloi24 <nhxloi24@gmail.com>
2023-10-05 19:48:14 +0700

    sc, 8

---
+++ bai8/bai8.js
@@ -0,0 +1,53 @@
+// bai 1
+
+// let valueVar = 10;
+// function updateValue(value){
+//     value = 20;
+//     return value;
+// }
+//  let updatedValue= updateValue(valueVar)
+//  console.log(updatedValue); // 20
+//  console.log(valueVar); // 10
+
+// const referenceVar = [1, 2, 3];
+// function updateArray(arr){
+//     if (arr.length > 0){
+//         arr[0] = 10;
+//     }
+// }
+// updateArray(referenceVar);
+// console.log('After:', referenceVar);
+
+//bai 2
+
+var arr = [1, 2, 3, 4, 5]
+
+// arr.forEach(function(e){
+//     console.log(e)
+// })
+
+// const result = arr.find(function(e){
+//     return e > 3;
+// });
+// console.log(result) // 4
+
+// const result1 = arr.some(function(e){
+//     return e > 5;
+// })
+
+// console.log(result1);
+
+// const result2 = arr.every(function(e){
+//     return typeof e === 'number' && !isNaN(e);
+// });
+// console.log(result2);
+
+// const squaredArr = arr.map(function(e){
+//     return e ** 2;
+// })
+// console.log(squaredArr);
+
+// const filterArr = arr.filter(function(e){
+//     return e % 2 === 0;
+// });
+// console.log(filterArr);
