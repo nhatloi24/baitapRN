990cfe2930b4d82510565f353721d3827e99bd86
nhatloi24 <nhxloi24@gmail.com>
2023-10-08 17:18:01 +0700

    source code, bt9

---
+++ baitap9/bt9.js
@@ -0,0 +1,38 @@
+// bai 1
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const sumEven = numbers.reduce((sum, num) => {
+//     if (num % 2 === 0 ){
+//         return sum + num;
+//     }
+//     return sum;
+// }, 0);
+
+// console.log(sumEven);
+
+
+
+//bai 2
+
+
+// const strings = ["Hello", " ", "world", "!"];
+// const combinedString = strings.join("");
+// console.log(combinedString);
+
+//bai 3
+
+
+// function Person(name, age, gender){
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+// }
+
+// Person.prototype.introduce = function(){
+//     console.log("Name:" + this.name);
+//     console.log("Age:" + this.age);
+//     console.log("Gender:" + this.gender);
+// }
+
+// const person1 = new Person("Nhat Loi", 23, "Male");
+// person1.introduce();
